f98f5d00ad7cfcb0954b34a6ca2070eea1af3d4e
Dipali <deepalideore2005@gmail.com>
2025-08-31 00:43:36 +0530

    gell-all-menu get-menu-bytype update-menu api done

--- models/admin/menuModel.js
+++ models/admin/menuModel.js
@@ -12,4 +12,42 @@ const createMenuItem = async (item) => {
   return result.insertId;
 };
 
-module.exports = { createMenuItem };
+// Get menu item by ID
+const getMenuItemById = async (id) => {
+  const [rows] = await db.query("SELECT * FROM Menu_Items WHERE Item_id = ?", [id]);
+  return rows[0];
+};
+
+// Check if item already exists
+const getMenuItemByName = async (name) => {
+  const [rows] = await db.query(`SELECT * FROM Menu_Items WHERE LOWER(name) = LOWER(?)`, [name]);
+  return rows.length ? rows[0] : null;
+};
+
+// Get all menu items
+const getAllMenuItems = async () => {
+  const [rows] = await db.execute("SELECT * FROM Menu_Items");
+  return rows;
+};
+
+// Get menu items by meal type
+const getMenuItemsByType = async (meal_type) => {
+  const [rows] = await db.execute(
+    "SELECT * FROM Menu_Items WHERE meal_type = ?",
+    [meal_type]
+  );
+  return rows;
+};
+
+// Update menu item
+const updateMenuItem = async (id, data) => {
+  const { name, description, price, meal_type, quantity, image_url } = data;
+  await db.query(
+    `UPDATE Menu_Items 
+     SET name=?, description=?, price=?, meal_type=?, quantity=?, image_url=? 
+     WHERE Item_id=?`,
+    [name, description, price, meal_type, quantity, image_url, id]
+  );
+};
+
+module.exports = { createMenuItem, getMenuItemByName, getAllMenuItems, getMenuItemsByType , getMenuItemById,updateMenuItem};
